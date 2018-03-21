f4142a028225b46e0a236d5002b1cfa01788bc42
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:44:20 -0500

    daniel

--- mate.cpp
+++ mate.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
-
 using namespace std;
-
+/*
 int main()
 {
-   int x=5;
-   int y=6;
+   int x;
+   int y;
+   cout<<"Ingrese un numero para x: ";
+   cin>>x;
+   cout<<"Ingrese un numero para y: ";
+   cin>>y;
    int z=x+y;
    int b=x*y;
    int c=x-y;
-   int h=x/y
-   cout<<" el primer numero es "<< x<< "el segundo numero es "<<y<<endl;
-   cout<<" la multiplicacion es"<< b<<endl;
-   cout<<" la resta es"<< c<<endl;
-   cout<<" la divisiones"<<h<<endl;
+   int h=x/y;
+   cout<<" el primer numero es "<< x <<endl;
+   cout<<" el segundo numero es "<< y <<endl;
+   cout<<" la multiplicacion es"<< b <<endl;
+   cout<<" la resta es"<< c <<endl;
+   cout<<" la divisiones"<< h <<endl;
 
    return 0;
+
+}
+*/
+    int main()
+{
+    float x;
+    cout<< "ingresar el radio del circulo";
+    cin>>x;
+    float area=3.1416*(x*x);
+    cout<<" el area de circulo es:"<< area;
+
+    return 0;
+
 }
